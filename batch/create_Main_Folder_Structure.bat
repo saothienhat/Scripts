1b9f29736f5c258f666d4dedf270a068b199a2ca
Sao Thien Hat <saothienhat@yahoo.com>
2015-09-11 13:04:16 +0700

    Batch Scripts

---
+++ batch/create_Main_Folder_Structure.bat
@@ -0,0 +1,19 @@
+
+mkdir COMPANY\Installation
+mkdir COMPANY\Passwords
+mkdir COMPANY\Guides
+mkdir COMPANY\Policies
+
+mkdir MUSIC
+mkdir MINE\Templates
+mkdir MINE\Scripts
+mkdir MINE\Icons
+mkdir PHOTO
+
+mkdir PROJECTS\SVN_CLIENT
+mkdir PROJECTS\SVN_Repository
+mkdir PROJECTS\GITHUB
+
+mkdir SHARE
+mkdir SOFTWARE
+mkdir TEMP
