d34da3ca5de8e76e1082cbd60e4c2bea20a77b28
sao thien hat <saothienhat@yahoo.com>
2015-10-01 22:16:00 +0700

    Add more folder
    Add more folder

--- batch/create_Project_Structure.bat
+++ batch/create_Project_Structure.bat
@@ -1,9 +1,12 @@
 
 mkdir 01_REQUIREMENT
 mkdir 02_DESIN
+mkdir 02_DESIN\Database
 mkdir 03_CODING
 mkdir 04_TEST
 mkdir 05_DATA
+mkdir 05_DATA\Database
+mkdir 05_DATA\Images
 mkdir 06_CONFIGURATION
 mkdir 07_HISTORY
 mkdir 08_DOCUMENT
